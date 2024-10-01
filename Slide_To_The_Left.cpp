1a928cc862dc196cb5ca4b47e124bd2f87eaf8a3
rafaellezama <lr43469@mail.harpercollege.edu>
2024-10-01 10:11:00 -0500

    Commit

--- Slide_To_The_Left.cpp
+++ Slide_To_The_Left.cpp
@@ -1,13 +1,47 @@
 #include <iostream>
 #include <vector>
+#include<algorithm>
 using namespace std;
 
-  vector<int> rotate_left(const vector<int>& numbers,int rotate_position){
+bool promptLeftorsRight(){
+  bool rotateLeft = false;
+  string value;
+  do{
+    cout << "Would you like to rotate (L)eft or (R)ight?: " <<endl;
+    cin >> value;
+   //  conver to lowercase
+    for (int i= 0; i <value.length(); i ++) value[i] = tolower(value[i]);
+    if(value == "left" || value == "l"){
+      rotateLeft = true;
+      break;
+    }
+    else if(value == "right" || value == "y"){
+      rotateLeft: false;
+      break;
+    }else{
+      cout <<"Enter right or left: ";
+    }
+  } while (true);
+  return rotate_left;
+}
+
+
+
+vector<int> rotate_left(const vector<int>& numbers,int rotate_position){
   vector<int> left_rotation;
   for(int i = 0; i < numbers.size();i++){
     left_rotation.push_back(numbers[(i + rotate_position) % numbers.size()]);
   }
   return left_rotation;
+}
+
+vector<int> rotate_right(const vector<int>& numbers,int rotate_position){
+  vector<int> right_rotation;
+  int size = numbers.size();
+  for(int i = 0; i < numbers.size();i++){
+    right_rotation.push_back(numbers[(i - rotate_position + size) % numbers.size()]);
+  }
+  return right_rotation;
 }
 
 int main(){ 
@@ -17,23 +51,33 @@ int main(){
   cin >> size;
 
   vector<int>numbers(size);
-
   for(int i=0; i < size; i++){
     cout << "Enter number "<< i + 1<< ": ";
     cin >> numbers[i];
   }
+
+
       //original array
   cout << "\toriginal array"<<endl;
   for(int i; i < numbers.size();i++){
     cout << "\t " << numbers[i];
   }
-
+  bool rotateLeft = promptYesNo();
       //left rotation display
-  vector<int> rotated = rotate_left(numbers, rotate_position);
-  cout << "\n  array rotated to the left"<<endl;;
-  for (int num : rotated) {
-    cout << "\t "<< num;
+  vector<int> rotated;
+  if (rotateLeft){
+    rotated = rotate_left(numbers, rotate_position);
+    cout << "Array rotated to the left: ";
+  }else{
+    rotated = rotate_right(numbers; rotate_position);
+    cout << "Array rotated to the right: ";
+  }
+
+
+  for(int num: rotated){
+    cout << num << " ";
   }
   cout << endl;
+
   return 0;
 }
